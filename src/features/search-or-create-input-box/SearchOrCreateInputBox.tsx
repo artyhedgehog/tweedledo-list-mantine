00ca3469dc8bd3a663e9ebafd4d3316fb9f30b3b
Artem Kolomytsev <AKolomytsev@alfabank.ru>
2026-07-26 01:00:41 +0300

    feat(input-box): item menu

--- src/features/search-or-create-input-box/SearchOrCreateInputBox.tsx
+++ src/features/search-or-create-input-box/SearchOrCreateInputBox.tsx
@@ -1,24 +1,59 @@
 import {
     ChangeEventHandler,
-    KeyboardEventHandler,
+    MouseEvent,
     MouseEventHandler,
     ReactNode,
     Ref,
+    useState,
 } from 'react';
-import { CloseButton, TextInput } from '@mantine/core';
+import { Box, CloseButton, TextInput } from '@mantine/core';
+import { ENTER_KEY } from '@/components/TodoMvc/constants';
+import { ITodo } from '@/components/TodoMvc/interfaces';
 import { useI18n } from '@/utils/strings';
 import { TodoItemMenu } from '../item-menu';
+import { getToggledPriority } from '../priority';
+
+const defaultState = {
+    completed: false,
+    archived: false,
+    priority: undefined,
+};
 
 export function SearchOrCreateInputBox(props: {
     ref: Ref<HTMLInputElement>;
     disabled: boolean;
-    onKeyDown: KeyboardEventHandler<HTMLInputElement>;
     value: string;
+    onCreate: (todo: Omit<ITodo, 'id'>) => void;
     onChange: ChangeEventHandler<HTMLInputElement>;
     onClearInput: MouseEventHandler<HTMLButtonElement>;
     leftSection: ReactNode;
 }) {
     const { t } = useI18n();
+
+    const [state, setState] =
+        useState<Omit<ITodo, 'id' | 'title'>>(defaultState);
+
+    const trimmedValue = props.value.trim();
+
+    function handleNewTodoKeyDown(event: React.KeyboardEvent) {
+        if (event.keyCode !== ENTER_KEY) {
+            return;
+        }
+
+        event.preventDefault();
+
+        if (trimmedValue) {
+            props.onCreate({
+                title: trimmedValue,
+                ...state,
+            });
+        }
+    }
+
+    function handleClearInput(event: MouseEvent<HTMLButtonElement>) {
+        setState(defaultState);
+        props.onClearInput(event);
+    }
 
     return (
         <div className="header">
@@ -28,36 +63,49 @@ export function SearchOrCreateInputBox(props: {
                 variant="unstyled"
                 className="search-bar"
                 placeholder={t('searchBar.placeholder')}
-                onKeyDown={
-                    // TODO move creation logic here
-                    props.onKeyDown
-                }
+                onKeyDown={handleNewTodoKeyDown}
                 value={props.value}
                 onChange={props.onChange}
                 leftSection={props.leftSection}
                 rightSection={
                     props.value ? (
-                        <CloseButton onClick={props.onClearInput} />
+                        <CloseButton onClick={handleClearInput} />
                     ) : undefined
                 }
                 size="xl"
                 autoFocus
             />
 
-            {props.value && (
-                <TodoItemMenu
-                    archived={false}
-                    priority={undefined}
-                    onDestroy={props.onClearInput}
-                    onArchive={
-                        // TODO create in archive
-                        () => {}
-                    }
-                    onSetPriority={
-                        // TODO create with priority: high
-                        () => {}
-                    }
-                />
+            {trimmedValue && (
+                <Box mr={-16} pt="xs" p="md">
+                    <TodoItemMenu
+                        archived={state.archived}
+                        priority={state.priority}
+                        onCreate={() => {
+                            props.onCreate({
+                                title: trimmedValue,
+                                ...state,
+                            });
+                        }}
+                        onArchive={() => {
+                            props.onCreate({
+                                title: trimmedValue,
+                                ...state,
+                                archived: true,
+                            });
+                        }}
+                        onSetPriority={() => {
+                            setState((previous) => {
+                                return {
+                                    ...previous,
+                                    priority: getToggledPriority(
+                                        previous.priority
+                                    ),
+                                };
+                            });
+                        }}
+                    />
+                </Box>
             )}
         </div>
     );
